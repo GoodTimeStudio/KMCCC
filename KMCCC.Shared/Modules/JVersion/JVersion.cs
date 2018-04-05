786613a0a04cc71fd7ada70e4fe32b26aaf4452e
MicroOwl <bestowlcity@outlook.com>
2018-04-05 20:52:17 +0800

    Support forge version json

--- KMCCC.Shared/Modules/JVersion/JVersion.cs
+++ KMCCC.Shared/Modules/JVersion/JVersion.cs
@@ -66,6 +66,11 @@
 
         [JsonProperty("downloads")]
         public JLibraryDownloadsInfo DownloadsInfo { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        public string[] checksums { get; set; }
     }
 
     public class JLibraryDownloadsInfo
